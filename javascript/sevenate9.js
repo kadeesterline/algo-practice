3a79b43ef114b2c202a0fc3988e6e843a7af906a
Kade <kadeesterline@gmail.com>
2022-06-24 12:51:13 -0700

    adds seven ate 9

---
+++ javascript/sevenate9.js
@@ -0,0 +1,32 @@
+// Seven ate 9
+//########
+//  Codewars
+//  https://www.codewars.com/kata/56fa3c5ce4d45d2a52001b3c/train/javascript
+//  Write a function that removes all 9's preceded and followed by 7's
+//########
+// Example
+// -------
+// Input: ("797123912")
+// Output: "77123912"
+//########
+// O(n)
+
+function sevenAte9(str) {
+  let temp = [];
+  let i = 0;
+  while (i < str.length) {
+    if (str[i] !== "9") {
+      temp = [...temp, str[i]];
+      i++;
+    } else {
+      if (str[i - 1] === "7" && str[i + 1] === "7") {
+        i++;
+      } else {
+        temp = [...temp, str[i]];
+        i++;
+      }
+    }
+  }
+  let newStr = temp.join("");
+  return newStr;
+}
